aeab5531dd7d4c3f28fac2f1c834334e1d9f2a2b
detierrez <detierrez@gmail.com>
2025-12-26 12:54:12 -0300

    Implement base header layout

--- src/modules/ui/components/header/index.js
+++ src/modules/ui/components/header/index.js
@@ -3,6 +3,5 @@ import "./style.css";
 export default function renderHeader() {
   const header = document.createElement("header");
   header.classList.add("header");
-  header.textContent = "Hello"
   return header;
 }
--- src/modules/ui/components/main/index.js
+++ src/modules/ui/components/main/index.js
@@ -3,6 +3,5 @@ import "./style.css";
 export default function renderMain() {
   const main = document.createElement("main");
   main.classList.add("main");
-  main.textContent = "World!"
   return main;
 }
